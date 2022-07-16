d7b09876a55850c355188cd3cbd02d01b7522883
XZM1415 <18556951750@163.com>
2022-07-16 17:08:57 +0800

    一个奇怪的异常

---
+++ mainpackage/CyxMotherHasDeadException.java
@@ -0,0 +1,7 @@
+package mainpackage;
+
+public class CyxMotherHasDeadException extends Exception{
+    public CyxMotherHasDeadException(){
+        super("喔唷，崩溃了，显示cyx妈时出现了问题，可能是cyx妈不存在");
+    }
+}
